e665e26822e3e0e12c7ccb230e5f6137598e6866
Andrew Templeton <andrw.tmplton@gmail.com>
2015-11-20 21:00:03 -0600

    v0.1.0; Tree resources generation support, wait sequences after delete correct

---
+++ README.md
@@ -0,0 +1,158 @@
+
+# cfn-api-gateway-resource-tree
+
+
+## Purpose
+
+AWS CloudFormation does not support AWS API Gateway. This is a Lambda-backed custom resource to add [AWS API Gateway's Resource](http://docs.aws.amazon.com/apigateway/api-reference/resource/resource/) to CloudFormation. This module allows construction of an arbitrarily large tree of resources. The standalone module for a single resource is `cfn-gateway-resource` ([NPM](https://www.npmjs.com/package/cfn-api-gateway-resource) / [GitHub](https://www.github.com/andrew-templeton/cfn-api-gateway-resource)).
+
+[This package on NPM](https://www.npmjs.com/package/cfn-api-gateway-resource)  
+[This package on GitHub](https://www.github.com/andrew-templeton/cfn-api-gateway-resource-tree)
+
+
+## Implementation
+
+This Lambda makes use of the Lambda-Backed CloudFormation Custom Resource flow module, `cfn-lambda` ([GitHub](https://github.com/andrew-templeton/cfn-lambda) / [NPM](https://www.npmjs.com/package/cfn-lambda)).
+
+
+## Usage
+
+  See [`./example.template.json`](./example.template.json) for a sample CloudFormation template. The example uses `Condition` statements, `Parameters`, and dynamic `ServiceToken` generation fully.
+
+
+    "ResourceLogicalIdInResourcesObject": {
+      "Type": "Type": "Custom::ApiGatewayResourceTree",
+      "Properties": {
+        "ServiceToken": "arn:aws:lambda:<cfn-region-id>:<your-account-id>:function:<this-deployed-lambda-name>",
+        "ParentId": {   // REQUIRED - can be any APIG Resource ID
+          "Fn::GetAtt": [       // This example uses the root of an API RestApi object.
+            "MyRestApi",
+            "RootResourceId"
+          ]
+        },
+        "RestApiId": {
+          "Ref": "MyRestApiId"  //  REQUIRED 10 char alphanum ID for RestApi
+        },
+        "ChildResources": <A ChildResources Object>, // REQUIRED see struct below.
+      }
+    }
+
+*CAUTION:* `ResourceTree` will destroy any other properties on the `ParentId` Resource.
+*CAUTION:* Any modification to any property triggers a full replacement.
+
+
+#### `ChildResources` Object
+
+Recursively nestable tree structure definition for your API Gateway structure.
+
+```
+[ 
+  {
+    "PathPart": "pathwithoutchildren"
+    // Do not need to include ChildResources.
+  },
+  {
+    "PathPart": "pathwithchildren",
+    "ChildResources": <A ChildResourcesObject>
+  },
+  {
+    "PathPart": "{pathparam}",
+    "ChildResources": `<a ChildResourcesObject>`
+  }
+]
+```
+
+####  `Ref` and `Fn::GetAtt` 
+
+`Fn::GetAtt` provides a key-value pair set for the ResourceId's of each path. There are two namespaces, the relative paths of the resources this object created, and those prefixed with `Absolute::`, which returns the absolute resource path on the `RestApi` object.
+
+```
+// This will return the `ResourceId` for the resource at relative path `/foo/{id}/bar`.
+// The root is defined as the `ParentResourceId`'s Path on the `RestApi` object.
+{"Fn::GetAtt": "/foo/{id}/bar"}
+// Now, example with `Absolute::` and a `ParentResource` at `/parent/resource`.
+{"Fn::GetAtt": "Absolute::/parent/resource/foo/{id}/bar"}
+// This will return the `ResourceId` for the resource relative to the API root,
+//   which is the `ResourceId` at `<APIROOT>/parent/resource/foo/{id}/bar`.
+```
+
+
+`Ref` returns a value only used for tracking internal state:
+```
+[
+  <RestApiId>,
+  <ParentResourceId>,
+  SHA256(JSON.stringify(<ChildResources>))
+].join('---')
+```
+
+#### Prerequisites for the Sample Template
+
+While the resource works on its own, the [`./example.template.json`](./example.template.json) requires use of some more custom resources...:
+
+ - `Custom::ApiGatewayRestApi` ([GitHub](https://github.com/andrew-templeton/cfn-api-gateway-restapi) / [NPM](https://www.npmjs.com/package/cfn-api-gateway-restapi))
+ - `Custom::ApiGatewayMethod` ([GitHub](https://github.com/andrew-templeton/cfn-api-gateway-method) / [NPM](https://www.npmjs.com/package/cfn-api-gateway-method))
+ - `Custom::ApiGatewayMethodResponse` ([GitHub](https://github.com/andrew-templeton/cfn-api-gateway-method-response) / [NPM](https://www.npmjs.com/package/cfn-api-gateway-method-response))
+ - `Custom::ApiGatewayIntegration` ([GitHub](https://github.com/andrew-templeton/cfn-api-gateway-integration) / [NPM](https://www.npmjs.com/package/cfn-api-gateway-integration))
+ - `Custom::ApiGatewayIntegrationResponse` ([GitHub](https://github.com/andrew-templeton/cfn-api-gateway-integration-response) / [NPM](https://www.npmjs.com/package/cfn-api-gateway-integration-response))
+ - `Custom::ApiGatewayDeployment` ([GitHub](https://github.com/andrew-templeton/cfn-api-gateway-deployment) / [NPM](https://www.npmjs.com/package/cfn-api-gateway-deployment))
+
+
+## Installation of the Resource Service Lambda
+
+#### Using the Provided Instant Install Script
+
+The way that takes 10 seconds...
+
+    # Have aws CLI installed + permissions for IAM and Lamdba
+    $ npm run cfn-lambda-deploy
+
+
+You will have this resource installed in every supported Region globally!
+
+
+#### Using the AWS Console
+
+... And the way more difficult way.
+
+*IMPORTANT*: With this method, you must install this custom service Lambda in each AWS Region in which you want CloudFormation to be able to access the `ApiGatewayResourceTree` custom resource!
+
+1. Go to the AWS Lambda Console Create Function view:
+  - [`us-east-1` / N. Virginia](https://console.aws.amazon.com/lambda/home?region=us-east-1#/create?step=2)
+  - [`us-west-2` / Oregon](https://console.aws.amazon.com/lambda/home?region=us-west-2#/create?step=2)
+  - [`eu-west-1` / Ireland](https://console.aws.amazon.com/lambda/home?region=eu-west-1#/create?step=2)
+  - [`ap-northeast-1` / Tokyo](https://console.aws.amazon.com/lambda/home?region=ap-northeast-1#/create?step=2)
+2. Zip this repository into `/tmp/ApiGatewayResourceTree.zip`
+
+    `$ cd $REPO_ROOT && zip -r /tmp/ApiGatewayResourceTree.zip;`
+
+3. Enter a name in the Name blank. I suggest: `CfnLambdaResouce-ApiGatewayResourceTree`
+4. Enter a Description (optional).
+5. Toggle Code Entry Type to "Upload a .ZIP file"
+6. Click "Upload", navigate to and select `/tmp/ApiGatewayResourceTree.zip`
+7. Set the Timeout under Advanced Settings to 10 sec
+8. Click the Role dropdown then click "Basic Execution Role". This will pop out a new window.
+9. Select IAM Role, then select option "Create a new IAM Role"
+10. Name the role `lambda_cfn_api_gateway_resource` (or something descriptive)
+11. Click "View Policy Document", click "Edit" on the right, then hit "OK"
+12. Copy and paste the [`./execution-policy.json`](./execution-policy.json) document.
+13. Hit "Allow". The window will close. Go back to the first window if you are not already there.
+14. Click "Create Function". Finally, done! Now go to [Usage](#usage) or see [the example template](./example.template.json). Next time, stick to the instant deploy script.
+
+
+#### Miscellaneous
+
+##### Collaboration & Requests
+
+Submit pull requests or Tweet [@ayetempleton](https://twitter.com/ayetempleton) if you want to get involved with roadmap as well, or if you want to do this for a living :)
+
+
+##### License
+
+[MIT](./License)
+
+
+##### Want More CloudFormation or API Gateway?
+
+Work is (extremely) active, published here:  
+[Andrew's NPM Account](https://www.npmjs.com/~andrew-templeton)
---
+++ index.js
@@ -0,0 +1,229 @@
+
+var crypto = require('crypto');
+
+var AWS = require('aws-sdk');
+var CfnLambda = require('cfn-lambda');
+
+var APIG = new AWS.APIGateway({apiVersion: '2015-07-09'});
+
+exports.handler = CfnLambda({
+  Create: Create,
+  Update: Update,
+  Delete: Delete,
+  SchemaPath: [__dirname, 'schema.json']
+});
+
+function Upsert(cleanable, makeable, reply) {
+  var idHash = {};
+  console.log('Sanitizing old tree: %j', cleanable);
+  cleanParent(cleanable.RestApiId, cleanable.ParentId,
+    function(cleanErr, cleanedPath) {
+      if (cleanErr) {
+        console.log('Failed to clean ParentId %s, FAILURE: %j',
+          cleanable.ParentId, cleanErr);
+        return reply(cleanErr.message);
+      }
+      console.log('Finished cleaning %s, now creating tree: %j', cleanedPath, makeable);
+      // Use root for building Relatives first, will map later to Absolute::
+      constructLayer(makeable.ParentId, '', makeable.ChildResources,
+        function(makeTreeErr, makeTreeSuccess) {
+          if (makeTreeErr) {
+            console.log('Failed to make tree, FAILURE: ', makeTreeErr);
+            return reply(makeTreeErr.message);
+          }
+          console.log('Made tree, now building GetAtt hash: %j', makeTreeSuccess);
+          Object.keys(idHash).forEach(function(relativePath) {
+            idHash[('Absolute::' + cleanedPath +
+              relativePath).replace('//', '/')] = idHash[relativePath];
+          });
+          console.log('Returning hash of GetAtt: %j', idHash);
+          reply(null, getPhysicalId(makeable), idHash);
+        });
+    });
+
+  function constructLayer(parentId, parentPath, children, completedLayer) {
+    asyncMap(children.map(function(child) {
+      var path = parentPath + '/' + child.PathPart;
+      return function(completedChild) {
+        makeResource(parentId, child.PathPart, function(err, childResult) {
+          if (err) {
+            console.log('Error making child %s: %j', path, err);
+            return completedChild(err);
+          }
+          console.log('Constructed resource %s: %j', path, child);
+          idHash[path] = childResult.id;
+          if (!child.ChildResources) {
+            return completedChild(null, childResult);
+          }
+          console.log('Child %s has children, constructing: %j', path, child.ChildResources);
+          constructLayer(childResult.id, path, child.ChildResources, completedChild);
+        });
+      };
+    }), function(layerErr, layerCompletion) {
+      if (layerErr) {
+        console.log('Layer construction error on %s: %j ',
+          parentPath, children);
+        return completedLayer(layerErr);
+      }
+      completedLayer(null, layerCompletion);
+    });
+  }
+
+  function makeResource(parentId, pathPart, callback) {
+    APIG.createResource({
+      restApiId: makeable.RestApiId,
+      parentId: parentId,
+      pathPart: pathPart
+    }, callback);
+  }
+}
+
+
+
+function Create(params, reply) {
+  console.log('CREATE delegating to Upsert.');
+  Upsert(params, params, reply);
+}
+
+function Update(physicalId, params, oldParams, reply) {
+  console.log('UPDATE delegating to Upsert.');
+  Upsert(oldParams, params, reply);
+}
+
+function Delete(physicalId, params, reply) {
+  if (physicalId === getPhysicalId(params)) {
+    console.log('Appears this is an UPDATE_CLEANUP, mismatched deletion signature.');
+    console.log('Non-deleted stream table: %j', params);
+    return reply();
+  }
+  cleanParent(params.RestApiId, params.ParentId, function(cleanErr, cleanedPath) {
+    if (cleanErr && cleanErr.statusCode !== 404) {
+      console.log('Failed to clean %s');
+      return reply(cleanErr.message);
+    }
+    console.log('Deleted the subtree: %s', cleanedPath);
+    reply();
+  });
+}
+
+function getPhysicalId(params) {
+  var shasum = crypto.createHash('sha256');
+  shasum.update(JSON.stringify(params.ChildResources));
+  return [
+    params.RestApiId,
+    params.ParentId,
+    shasum.digest('hex')
+  ].join('---');
+}
+
+function cleanParent(restApiId, parentResourceId, callback) {
+
+  var pageSize = 500;
+  var resources = [];
+  collectResources();
+
+  function collectResources(position) {
+    console.log('Collecting resources on the API...');
+    APIG.getResources({
+      restApiId: restApiId,
+      limit: pageSize,
+      position: position
+    }, function(err, data) {
+      if (err) {
+        console.error('Error while cleaning parent resource: %j', err);
+        return callback(err);
+      }
+      resources = resources.concat(data.items);
+      // Might need to recurse...
+      if (data.items.length === pageSize) {
+        console.log('Found more than one page of %s for resources.', pageSize);
+        return collectResources(position);
+      }
+      continueWithCleanse();
+    });
+  }
+
+  function continueWithCleanse() {
+    console.log('Found batch of Resource objects: %j', resources);
+    var parentPath = resources.filter(function(resource) {
+      return resource.id === parentResourceId;
+    })[0].path;
+    var immediateChildResources = resources.filter(function(resource) {
+      return resource.path !== '/' && resource.path.indexOf(parentPath) === 0 &&
+        parentPath.replace(/^\//, '').split('/').length + 1 ===
+          resource.path.split('/').length;
+    });
+    console.log('Found batch of immediate child Resource ' +
+      'objects, commence deletion: %j', immediateChildResources);
+    asyncMap(immediateChildResources.map(function(resource) {
+      return deleteResource.bind(null, restApiId, resource.id);
+    }), function(deleteAllErr, results) {
+      if (deleteAllErr) {
+        console.error('Error when deleting child resource set: %j', deleteAllErr);
+        return callback(deleteAllErr);
+      }
+      console.log('Cleaned parent resource: %s', parentPath);
+      callback(null, parentPath);
+    });
+  }
+}
+
+function deleteResource(restApiId, resourceId, callback) {
+  APIG.deleteResource({
+    restApiId: restApiId,
+    resourceId: resourceId
+  }, function(err, data) {
+    if (err && err.statusCode !== 404) {
+      console.log('Failed to delete resource: %j', err);
+      return callback(err);
+    }
+    console.log('Initialized cleanse resource %s: %j', resourceId, data);
+    waitFor404(restApiId, resourceId, callback);
+  });
+}
+
+function waitFor404(restApiId, resourceId, callback) {
+  console.log('Waiting for %s to actually delete...', resourceId);
+  APIG.getResource({
+    restApiId: restApiId,
+    resourceId: resourceId
+  }, function(err, data) {
+    if (err && err.statusCode === 404) {
+      console.log('Resource %s actually deleted.', resourceId);
+      return callback();
+    }
+    if (err) {
+      console.error('Fatal error while waiting for resource %s to delete.', resourceId);
+      return callback(err);
+    }
+    setTimeout(function() {
+      waitFor404(restApiId, resourceId, callback);
+    }, 100);
+  });
+}
+
+function asyncMap(actionSet, callback) {
+  var results = [];
+  var failed = false;
+  var completed = 0;
+  if (!actionSet.length) {
+    return callback(null, []);
+  }
+  actionSet.forEach(function(action, index) {
+    action(function(err, data) {
+      if (failed) {
+        return;
+      }
+      if (err) {
+        failed = true;
+        return callback(err);
+      }
+      results[index] = data;
+      completed++;
+      if (completed === actionSet.length) {
+        callback(null, results);
+      }
+    });
+  });
+}
+
